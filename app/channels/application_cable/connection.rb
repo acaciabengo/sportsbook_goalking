ebf357767587b9097b23471817edd26885bf6e47
Acacia Ssembajjwe <acaciabengo@gmail.com>
2026-01-05 12:06:52 +0300

    bug fix cable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,24 +4,30 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = current_user
+      self.current_user = find_verified_user
+      Rails.logger.info "ActionCable connected: #{current_user&.id || 'guest'}"
+    end
+
+    def receive(data)
+      Rails.logger.info "RAW DATA RECEIVED: #{data.inspect}"
+      super
     end
 
     private
 
-    def current_user
-      return @current_user if defined?(@current_user)
-
+    def find_verified_user
       auth_header = request.headers['Authorization']
       token = auth_header&.split(' ')&.last if auth_header.present?
-      return @current_user = nil unless token
+      return nil unless token
 
       begin
         decoded_token = JWT.decode(token, ENV['DEVISE_JWT_SECRET_KEY'], true, algorithm: 'HS256')
         payload = decoded_token.first
-        @current_user = User.find_by(id: payload['sub'])
-      rescue JWT::DecodeError, JWT::ExpiredSignature
-        @current_user = nil
+        user = User.find_by(id: payload['sub'])
+        user
+      rescue JWT::DecodeError, JWT::ExpiredSignature => e
+        Rails.logger.error "JWT Error: #{e.message}"
+        nil
       end
     end
   end
