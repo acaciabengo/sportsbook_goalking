f098ef74fd2b4b980e1271f32844ab37b8a4cc05
Acacia Ssembajjwe <acaciabengo@gmail.com>
2026-01-31 16:24:55 +0300

    bug fix dual auth

--- lib/devise/models/dual_authenticatable.rb
+++ lib/devise/models/dual_authenticatable.rb
@@ -1,32 +1,59 @@
 # frozen_string_literal: true
 
 require 'devise/strategies/dual_authenticatable'
+require_relative '../../dotnet_password_hasher'
 
 module Devise
   module Models
     module DualAuthenticatable
       extend ActiveSupport::Concern
 
-      # TODO: Add any model-level methods needed for your authentication logic
-      #
-      # Example class methods:
-      #   module ClassMethods
-      #     def find_for_dual_authentication(conditions)
-      #       find_for_authentication(conditions)
-      #     end
-      #   end
-      #
-      # Example instance methods:
-      #   def valid_for_dual_authentication?(password)
-      #     valid_password?(password)
-      #   end
-
       included do
-        # Add any callbacks or validations here
+        attr_reader :password, :current_password
+        attr_accessor :password_confirmation
+      end
+
+      # Verifies password and migrates legacy .NET passwords to bcrypt
+      def valid_password?(password)
+        return false if encrypted_password.blank?
+
+        if legacy_password?
+          if DotnetPasswordHasher.verify(password, encrypted_password)
+            # Migrate to bcrypt on successful login
+            self.password = password
+            self.legacy_password = false
+            save(validate: false)
+            true
+          else
+            false
+          end
+        else
+          Devise::Encryptor.compare(self.class, encrypted_password, password)
+        end
+      end
+
+      # Sets password and hashes it with bcrypt
+      def password=(new_password)
+        @password = new_password
+        self.encrypted_password = password_digest(new_password) if new_password.present?
+      end
+
+      # Generates bcrypt digest
+      def password_digest(password)
+        Devise::Encryptor.digest(self.class, password)
+      end
+
+      # Required by Devise for authentication
+      def authenticatable_salt
+        encrypted_password[0, 29] if encrypted_password
       end
 
       module ClassMethods
-        # Add class methods here
+        def find_for_authentication(conditions)
+          find_by(conditions)
+        end
+
+        Devise::Models.config(self, :pepper, :stretches)
       end
     end
   end
